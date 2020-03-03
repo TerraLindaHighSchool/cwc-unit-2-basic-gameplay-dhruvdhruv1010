f9a9b8d87a77802474f26780c440691b533451b3
Dhruv Pareek <dp00888@srcs.loc>
2020-03-03 14:29:17 -0800

    Lab 4.3 c

--- Dhruv Pareek Personal Project/Assets/Scripts/MoveDown.cs
+++ Dhruv Pareek Personal Project/Assets/Scripts/MoveDown.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveDown : MonoBehaviour
 {
     public float speed = 5.0f;
+    private float zDestroy = -10.0f;
     private Rigidbody objectRb;
     
     // Start is called before the first frame update
@@ -17,5 +18,10 @@ public class MoveDown : MonoBehaviour
     void Update()
     {
         objectRb.AddForce(Vector3.forward * -speed);
+
+        if(transform.position.z < zDestroy)
+        {
+            Destroy(gameObject);
+        }
     }
 }
