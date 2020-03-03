d3b221ec52be3f81e2c8b41ac29879e310fe7340
Dhruv Pareek <dp00888@srcs.loc>
2020-03-03 14:10:42 -0800

    Lab 3.4 c

--- Dhruv Pareek Personal Project/Assets/Scripts/PlayerController.cs
+++ Dhruv Pareek Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float zBound = 6;
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -16,10 +17,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if(transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }
