cb3d5f674489f7709ff743e5dd305a11dcf901cc
Dhruv Pareek <dp00888@srcs.loc>
2020-03-03 15:35:25 -0800

    Lab 4.6 started

--- Dhruv Pareek Personal Project/Assets/Scripts/SpawnManager.cs
+++ Dhruv Pareek Personal Project/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemies;
-    public GameObject powerup;    
+    public GameObject powerup;
+
+    private float zEnemySpawn = 12.0f;
+    private float xSpawnRange = 16.0f;
+    private float zPowerupRange = 5.0f;
+    private float ySpawn = 0.75f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
     }
 }
