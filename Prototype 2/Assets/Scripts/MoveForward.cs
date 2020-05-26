186d1555bdcb9a55c61e58cd78546fe1e17827f8
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-05-25 17:18:48 -0700

    Cars move forward

--- Prototype 2/Assets/Scripts/MoveForward.cs
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -14,6 +14,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate((1, 0, 0) * Time.deltaTime * speed);
     }
 }
